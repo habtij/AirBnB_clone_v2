b680db029cb7ba8b36d9a3fd909a1c4e7f246b5d
Habeeb <tijanihabeeb12@outlook.com>
2024-04-18 19:42:29 +0100

    Update: 1. HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Define the route for the root url '/'
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Return 'Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ Return HBNB """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    # Start the Flask development server
+    # Listen on all available networj interfaces (0.0.0.0) and port 5000
+    app.run(host='0.0.0.0', port=5000)
