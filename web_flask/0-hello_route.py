9c7019f57de4754005ec19f74b2f5ae7d251d765
Habeeb <tijanihabeeb12@outlook.com>
2024-04-18 19:21:01 +0100

    Add: 0. Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Define the route for the root url '/'
+@app.route('/', strict_slahes=False)
+def hello_hbnb():
+    """ Displays 'Hello HBNB! """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    # Start the Flask development server
+    # Listen on all available networj interfaces (0.0.0.0) and port 5000
+    app.run(host='0.0.0.0', port=5000)
